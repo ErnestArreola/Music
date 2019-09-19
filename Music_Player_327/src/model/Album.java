d3ade77a5e2760329f3c61a4fff550fa05cf3b78
Ernest <arreern@gmail.com>
2019-09-19 06:09:39 -0700

    Working: Except (json, rename).

---
+++ Music_Player_327/src/model/Album.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author AppleSauce
+ */
+public class Album {
+    
+    
+   public int id; 
+   public String name;
+  
+    
+
+
+public Album() {
+
+
+}
+
+
+}
