77584d73fdeacbd8ab0d500bd49fbea9940672b6
Ernest <arreern@gmail.com>
2019-10-03 11:44:51 -0700

    Again Main

---
+++ Music_Player_327/src/model/Album.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author AppleSauce
+ */
+public class Album {
+    
+    
+   public int id; 
+   public String name;
+  
+    
+
+
+public Album(String name) {
+    this.name = name;
+}
+
+public String getName(){
+    return name;
+}
+
+
+
+}
