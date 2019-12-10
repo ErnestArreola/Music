d73508919e3ad5e21ceb12d2f90c2ec55b5c4234
ErnestArreola <47127673+ErnestArreola@users.noreply.github.com>
2019-12-10 12:59:17 -0800

    First

---
+++ Temp_Music_Player/Transaction.java
@@ -0,0 +1,79 @@
+
+package chord;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author applesauce
+ */
+public class Transaction implements Serializable {
+
+
+  public enum Operation {WRITE, DELETE}
+
+  public enum Vote {YES, NO}
+
+  Long TransactionId;
+
+  Vote vote;
+  
+  String fileName;
+  
+  long pageIndex;
+  
+  long guid;
+
+  Operation operation;
+
+  RemoteInputFileStream fileStream;
+
+
+  public Transaction(long g, RemoteInputFileStream fileStream, Long Id, Operation operation) {
+
+    this.guid = g;
+
+    this.fileStream = fileStream;
+
+    this.TransactionId = Id;
+
+    this.operation = operation;
+  }
+
+  public void voteYes() {
+    vote = Vote.YES;
+  }
+
+
+  public void voteNo() {
+    vote = Vote.NO;
+
+  }
+
+
+  public Vote getVote() {
+    return vote;
+
+  }
+
+  public Operation getOperation() {
+
+    return operation;
+
+  }
+
+
+  public long getFileId() {
+    return guid;
+  }
+
+  public Long getTransactionId() {
+     return TransactionId;
+  }
+
+  public RemoteInputFileStream getStream() {
+    return fileStream;
+
+  }
+
+}
